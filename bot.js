1329f2a6e4ccae1f41a504f58d32ae8bf9f91891
Leveofan1197 <Programma1197>
2020-12-31 23:37:20 +0500

    Initial commit

---
+++ bot.js
@@ -0,0 +1,29 @@
+import Telegraf from 'telegraf'
+import covidApi from 'covid19-api'
+const bot = new Telegraf('1472312554:AAH_CT2GoTdSsYMdPGRLmdgG2bFvz916BDU') //сюда помещается токен, который дал botFather
+import { getMainMenu } from './keyboard.js'
+
+bot.start( ctx => ctx.reply(`
+   Привет ${ctx.from.first_name}!
+   Узнай статистику по Коронавирусу.
+   Введи страну на английском языке и получи статистику.
+   Получить весь список стран можно по команде /help."
+`))
+
+bot.help( ctx => ctx.reply('Выбери из списка страну', getMainMenu())) // список всех стран на английском языке можно взять в документации covid19-api
+bot.on('text', async (ctx) => {
+    try {
+        const userText = ctx.message.text
+        const covidData = await covidApi.getReportsByCountries(userText)
+        const countryData = covidData[0][0]
+        const formatData = `
+           Страна: ${countryData.country},
+           Случаи: ${countryData.cases},
+           Смерти: ${countryData.deaths},
+           Выздоровело: ${countryData.recovered}`
+        await ctx.reply(formatData)
+    } catch(e) {
+        ctx.reply('Такой страны не существует, для получения списка стран используй команду /help')
+    }
+})
+bot.launch()
